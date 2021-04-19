be17ba8eb6a222e22da5fa7435705963d8942c5a
nando <muhammad.rezki.ananda@gmail.com>
2021-04-19 21:32:17 +0700

    Custom TabBar

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -3,7 +3,9 @@ import 'package:bwa_foodmarket/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
+part 'custom_tabbar.dart';
