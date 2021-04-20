898c283f4432ee63723f76c5c1a9360b2d1e02f8
nando <muhammad.rezki.ananda@gmail.com>
2021-04-20 09:11:32 +0700

    Food List Item

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:supercharged/supercharged.dart';
+import 'package:intl/intl.dart';
 
 part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
 part 'custom_tabbar.dart';
+part 'food_list_item.dart';
