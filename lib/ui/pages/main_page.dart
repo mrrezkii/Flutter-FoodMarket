7db7d5dbc92d51d43240854ffad311513fe8f25a
nando <muhammad.rezki.ananda@gmail.com>
2021-04-21 22:08:27 +0700

    Order History Page (1)

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -32,16 +32,7 @@ class _MainPageState extends State<MainPage> {
                   },
                   children: <Widget>[
                 Center(child: FoodPage()),
-                Center(
-                    child: IllustrationPage(
-                  title: 'Ouch! Hungry',
-                  subtitle: 'Seems you like have not\nordered any food yet',
-                  picturePath: 'assets/love_burger.png',
-                  buttonTap1: () {},
-                  buttonTitle1: 'Find Foods',
-                  buttonTap2: () {},
-                  buttonTitle2: 'Oke',
-                )),
+                Center(child: OrderHistoryPage()),
                 Center(child: Text("Profile"))
               ])),
           Align(
