fd007f183642559258abafe8a61a4bb34373dcaf
nando <muhammad.rezki.ananda@gmail.com>
2021-04-25 08:45:27 +0700

    Food Market Services

---
+++ lib/services/services.dart
@@ -0,0 +1,6 @@
+import 'package:bwa_foodmarket/models/models.dart';
+import 'package:bwa_foodmarket/models/models.dart';
+
+part 'user_services.dart';
+part 'food_services.dart';
+part 'transaction_services.dart';
